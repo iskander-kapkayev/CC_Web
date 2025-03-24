bda80eeb49445d8154e8fa3cc2f0dc8dc6c60fe5
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-03-24 15:26:33 -0400

    multiple quotes

--- scripts/script.js
+++ scripts/script.js
@@ -272,7 +272,7 @@ function displayCaptions(currentCaptions) {
                     <span id='postCaption'>${post.captiontext}</span>
                     <span id='postUser'> - ${post.username} </span>
                 </span>
-                <div data-info='{'data': [${post.captiontext}, ${post.username}]}' id='postUpvotes'>
+                <div data-info='{"data": [${post.captiontext}, ${post.username}]}' id='postUpvotes'>
                     <a onclick='uservote(this.closest('div').getAttribute('data-info'))'>&#x2764</a> ${post.votecount}
                 </div>
             `;
