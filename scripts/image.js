57370b8fb0b51ca3a3d99b3816819944ebb52e59
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-04-02 14:23:23 -0400

    added an image.js

---
+++ scripts/image.js
@@ -0,0 +1,10 @@
+/* 
+import images as import values
+*/
+
+import img_0 from './images/fire_alarm_deserted_island.jpg';
+import img_1 from './images/fun_shirt_island.jpg';
+import img_2 from './images/visiting_to_island.jpg';
+import img_3 from './images/anime_1.jpg';
+import img_4 from './images/anime_2.jpg';
+import img_5 from './images/anime_3.jpg';
--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,6 @@ Universal URL to access server
 const webURL = 'https://cc-web-iota.vercel.app';
 const servURL = 'https://cc-server-lake.vercel.app';
 
-/* 
-import images as import values
-*/
-
-import img_0 from './images/fire_alarm_deserted_island.jpg';
-import img_1 from './images/fun_shirt_island.jpg';
-import img_2 from './images/visiting_to_island.jpg';
-import img_3 from './images/anime_1.jpg';
-import img_4 from './images/anime_2.jpg';
-import img_5 from './images/anime_3.jpg';
 
 /*
 Async function call to fetch http request.
