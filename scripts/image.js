95a379671a5e7884e0bb080bc7e12301c78d9e69
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-04-02 14:25:44 -0400

    adjusted url

--- scripts/image.js
+++ scripts/image.js
@@ -2,9 +2,11 @@
 import images as import values
 */
 
-import img_0 from './images/fire_alarm_deserted_island.jpg';
-import img_1 from './images/fun_shirt_island.jpg';
-import img_2 from './images/visiting_to_island.jpg';
-import img_3 from './images/anime_1.jpg';
-import img_4 from './images/anime_2.jpg';
-import img_5 from './images/anime_3.jpg';
+const webURL = 'https://cc-web-iota.vercel.app';
+
+import img_0 from `${webURL}/images/fire_alarm_deserted_island.jpg`;
+import img_1 from `${webURL}/images/fun_shirt_island.jpg`;
+import img_2 from `${webURL}/images/visiting_to_island.jpg`;
+import img_3 from `${webURL}/images/anime_1.jpg`;
+import img_4 from `${webURL}/images/anime_2.jpg`;
+import img_5 from `${webURL}/images/anime_3.jpg`;
