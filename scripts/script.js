d1f9a7e19cf2ae094804f8d0c425f164e0bcc4f4
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-04-02 14:17:11 -0400

    import images for image stability

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,17 @@ Universal URL to access server
 const webURL = 'https://cc-web-iota.vercel.app';
 const servURL = 'https://cc-server-lake.vercel.app';
 
+/* 
+import images as import values
+*/
+
+import img_0 from './images/fire_alarm_deserted_island.jpg';
+import img_1 from './images/fun_shirt_island.jpg';
+import img_2 from './images/visiting_to_island.jpg';
+import img_3 from './images/anime_1.jpg';
+import img_4 from './images/anime_2.jpg';
+import img_5 from './images/anime_3.jpg';
+
 /*
 Async function call to fetch http request.
 One will be for gets.
@@ -71,7 +82,7 @@ async function moveToImageNext() {
     let currentIndex = Number(localStorage.getItem('currentIndex'));
 
     currentIndex = (currentIndex + 1) % storedArray.length;
-    img.src = storedArray[currentIndex];
+    img.src = `{img_${currentIndex}}`;
     img.alt = `index ${currentIndex}`;
 
     localStorage.setItem('currentIndex', currentIndex); // set new currentIndex
@@ -104,7 +115,7 @@ async function moveToImagePrev() {
         // currentInfex is not -1
         currentIndex = (currentIndex) % storedArray.length;
     }
-    img.src = storedArray[currentIndex];
+    img.src = `{img_${currentIndex}}`;
     img.alt = `index ${currentIndex}`;
 
     localStorage.setItem('currentIndex', currentIndex); // set new currentIndex
@@ -133,7 +144,7 @@ async function assignImage() {
     // set the first image on start up
     let img = document.getElementById('myImage');
     const currentIndex = Number(localStorage.getItem('currentIndex'));
-    img.src = imageURLs[currentIndex];
+    img.src = `{img_${currentIndex}}`;
     img.alt = `index ${currentIndex}`;
     await collectCaptions();
 }
@@ -676,3 +687,4 @@ window.addEventListener('load', function() {
     loaderWrapper.style.display = 'none';
 
 });
+
