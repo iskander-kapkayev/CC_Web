699660b3a1597ed2a86afb07a5d598413dcb349b
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-04-02 14:27:02 -0400

    changed off of const

--- scripts/image.js
+++ scripts/image.js
@@ -2,11 +2,9 @@
 import images as import values
 */
 
-const webURL = 'https://cc-web-iota.vercel.app';
-
-import img_0 from `${webURL}/images/fire_alarm_deserted_island.jpg`;
-import img_1 from `${webURL}/images/fun_shirt_island.jpg`;
-import img_2 from `${webURL}/images/visiting_to_island.jpg`;
-import img_3 from `${webURL}/images/anime_1.jpg`;
-import img_4 from `${webURL}/images/anime_2.jpg`;
-import img_5 from `${webURL}/images/anime_3.jpg`;
+import img_0 from 'https://cc-web-iota.vercel.app/images/fire_alarm_deserted_island.jpg';
+import img_1 from 'https://cc-web-iota.vercel.app/images/fun_shirt_island.jpg';
+import img_2 from 'https://cc-web-iota.vercel.app/images/visiting_to_island.jpg';
+import img_3 from 'https://cc-web-iota.vercel.app/images/anime_1.jpg';
+import img_4 from 'https://cc-web-iota.vercel.app/images/anime_2.jpg';
+import img_5 from 'https://cc-web-iota.vercel.app/images/anime_3.jpg';
