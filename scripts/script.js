16000db51bee9b40d5ff3119d8fa6424a0fa07b4
Iskander Kapkayev <158784862+iskander-kapkayev@users.noreply.github.com>
2025-04-06 13:04:46 -0400

    added spacing for a delete included icon

--- scripts/script.js
+++ scripts/script.js
@@ -414,8 +414,8 @@ function displayCaptionsUser(currentCaptions, thisusername) {
                     <span id="postCaption">${currentCaptions[`${i}`].captiontext}</span>
                     <span id="postUser"> - ${currentCaptions[`${i}`].username} </span>
                 </span>
-                <div id="postUpvotes">
-                    <span class="deletion"> <a onclick="tester()"><i class="material-symbols-outlined">delete</i></a></span>
+                <div id="postUpvotesDelete">
+                    <span class="deletion"> <a onclick="tester()"><i id="deleteicon" class="material-symbols-outlined">delete</i></a></span>
                     <span class="heart"> <a onclick="userdownvote(JSON.parse(this.closest("div").getAttribute("data-info")))"><i id="downvoteheart" class="material-symbols-outlined">heart_minus</i></a></span>
                     <span class="votenum">${currentCaptions[`${i}`].votecount}</span>
                     <span class="heart"> <a onclick="userupvote(JSON.parse(this.closest("div").getAttribute("data-info")))"><i id="upvoteheart" class="material-symbols-outlined">heart_plus</i></a></span>
